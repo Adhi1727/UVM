b9597e36e907ad73d4ee040b66cbd9a3cf9e0851
ADHIKESAVAN . M <adhik8108@gmail.com>
2026-02-04 09:30:11 +0530

    Update 05.Sequencer.sv

--- Verifications_In_UVM/Full_Adder_Verification/05.Sequencer.sv
+++ Verifications_In_UVM/Full_Adder_Verification/05.Sequencer.sv
@@ -1,1 +1,9 @@
-
+class fa_sequencer extends uvm_sequencer #(fa_sequence_item);
+  `uvm_component_utils(fa_sequencer);
+  
+  function new(string name = "sequencer",uvm_component parent);
+    super.new(name,parent);
+    `uvm_info("SEQ_R","Sequencer created",UVM_LOW);
+  endfunction
+endclass
+  
