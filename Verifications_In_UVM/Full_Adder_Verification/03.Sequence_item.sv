acb39891fbf91e19b1de1954de8c3054921c4e37
ADHIKESAVAN . M <adhik8108@gmail.com>
2026-02-04 09:28:40 +0530

    Update 03.Sequence_item.sv

--- Verifications_In_UVM/Full_Adder_Verification/03.Sequence_item.sv
+++ Verifications_In_UVM/Full_Adder_Verification/03.Sequence_item.sv
@@ -1,16 +1,14 @@
-class fa_sequence extends uvm_sequence #(fa_sequence_item);
-  `uvm_object_utils(fa_sequence);
+class fa_sequence_item extends uvm_sequence_item;
+  `uvm_object_utils(fa_sequence_item);
   
-  function new(string name = "fa_sequence");
+  rand bit a;
+  rand bit b;
+  rand bit cin;
+  
+  bit sum;
+  bit cout;
+  
+  function new(string name = "fa_sequence_item");
     super.new(name);
   endfunction
-  
-  task body();
-    fa_sequence_item req;
-    `uvm_info("SEQ","sequence started",UVM_LOW);
-    repeat(5) begin
-      req = fa_sequence_item::type_id::create("req");
-      `uvm_do(req);
-    end
-  endtask
 endclass
